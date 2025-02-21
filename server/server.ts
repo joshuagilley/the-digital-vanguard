c2f67ce995695476796ec5dc4eb2c829690b79e3
Joshua Gilley <joshuadgilley@Joshuas-Mac-mini.local>
2025-02-21 18:13:36 -0500

    feat(express server to fastify because its more maintained and faster): express to fastify

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,34 @@
-// https://dev.to/mr_ali3n/folder-structure-for-nodejs-expressjs-project-435l
-// For reference when developing this further
-import express, { Request, Response } from "express";
-const app = express();
-import cors from "cors";
+require("dotenv").config();
+import Fastify from "fastify";
+const server = Fastify({ logger: true });
 
-const corsOptions = {
-  origin: ["http://localhost:5173"],
+server.addHook("onRequest", async (request, reply) => {
+  reply.header("Access-Control-Allow-Origin", process.env.ALLOW_ORIGIN);
+  reply.header("Access-Control-Allow-Credentials", true);
+  reply.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, X-Requested-With, Content-Type, Accept, X-Slug, X-UID"
+  );
+  reply.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, POST, PUT, PATCH, GET, DELETE"
+  );
+  if (request.method === "OPTIONS") {
+    reply.send();
+  }
+});
+
+server.get("/api", async (request, reply) => {
+  reply.send({ fruits: ["apple", "orange", "banana"] });
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 8080 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 };
 
-app.use(cors(corsOptions));
-
-app.get("/api", cors(corsOptions), (req: Request, res: Response) => {
-  res.json({ fruits: ["apple", "orange", "banana"] });
-});
-
-app.listen(8080, () => {
-  console.log("Server started on port 8080..");
-});
+start();
