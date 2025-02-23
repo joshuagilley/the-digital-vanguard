b0bebf16edd08d3e37e49e420196d00e751035ae
Joshua Gilley <joshuadgilley@Joshuas-Mac-mini.local>
2025-02-22 21:54:17 -0500

    feat: name change

--- server/config/register.ts
+++ server/config/register.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { Server } from "http";
 import articleRoutes from "routes/articles";
-import dbConnector from "config";
+import dbConnector from "config/db";
 
 const fastifyRegister = (fastify: FastifyInstance<Server>) => {
   fastify.register(dbConnector); // register mongodb db connection
