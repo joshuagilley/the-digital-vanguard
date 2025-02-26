bdc72fdef2e0b2b1c7997ec64507f2016649b2d0
Joshua Gilley <joshuadgilley@Joshuas-Mac-mini.local>
2025-02-25 20:22:41 -0500

    feat: added dropdown

--- client/src/pages/home/Home.test.tsx
+++ client/src/pages/home/Home.test.tsx
@@ -1,9 +1,31 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 import HomePage from "pages/home";
+import { vi } from "vitest";
 
 describe("Home Page", () => {
+  vi.mock("react-router-dom", async () => {
+    const actual = await vi.importActual("react-router-dom");
+    return {
+      ...actual,
+      useParams: vi.fn(),
+      useNavigate: vi.fn(),
+    };
+  });
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false, // Disable retries in tests
+      },
+    },
+  });
   test("renders ExampleComponent with correct text", () => {
-    render(<HomePage />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <HomePage />
+      </QueryClientProvider>
+    );
     const textElement = screen.getByTestId("home-page");
     expect(textElement).toBeInTheDocument();
   });
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,16 +1,87 @@
-import { Box, Center, Image } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Center,
+  Image,
+  Select,
+  Skeleton,
+  Stack,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { t } from "i18next";
+import { useNavigate } from "react-router-dom";
+import { User } from "types/user";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: [],
+    queryFn: async () => {
+      const response = await fetch(`/api/users`);
+      return await response.json();
+    },
+  });
+
+  const handleNavigate = (e: React.ChangeEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    navigate(`/portfolio/${value}`);
+  };
+
   return (
-    <Box
-      data-testid="home-page"
-      sx={{ backgroundImage: "matrix.jpg", backgroundSize: "xs" }}
-    >
-      <Center>
-        <Image src="tdv.png" />
-      </Center>
+    <Box data-testid="home-page" sx={styles.wrapper}>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>{t("portfolio.userDataFetchFail")}</AlertTitle>
+          <AlertDescription>{t("portfolio.tryAgain")}</AlertDescription>
+        </Alert>
+      )}
+      {(isPending || isFetching) && (
+        <Stack>
+          <Skeleton height="20px" />
+          <Skeleton height="20px" />
+          <Skeleton height="20px" />
+        </Stack>
+      )}
+      {!isPending && !isFetching && !error && (
+        <Box
+          data-testid="home-page"
+          sx={{ backgroundImage: "matrix.jpg", backgroundSize: "xs" }}
+        >
+          <Center>
+            <Image src="tdv.png" />
+          </Center>
+          <Box sx={styles.dropdown}>
+            <Select
+              placeholder="Select a dev"
+              onChange={handleNavigate}
+              variant="filled"
+            >
+              {data.map(({ username, userId }: User, index: number) => (
+                <option value={userId} key={index}>
+                  {username}
+                </option>
+              ))}
+            </Select>
+          </Box>
+        </Box>
+      )}
     </Box>
   );
+};
+
+const styles = {
+  wrapper: {
+    height: "100vh",
+  },
+  dropdown: {
+    position: "absolute",
+    top: "75px",
+    margin: "10px",
+  },
 };
 
 export default Home;
