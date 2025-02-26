4ddf1e54ce34ab0e174ae8758ecaddc8922d7183
Joshua Gilley <joshuadgilley@Joshuas-Mac-mini.local>
2025-02-26 12:33:00 -0500

    feat: finished added testing will add more later and clean up

--- server/server.test.ts
+++ server/server.test.ts
@@ -1,21 +1,29 @@
-import { FastifyInstance } from "fastify";
-import createServer from "./server";
+import { describe, it, expect } from "vitest";
 import supertest from "supertest";
+import createServer from "./server";
 
-describe("Server tests", () => {
-  let server: FastifyInstance;
-
-  beforeAll(async () => {
-    server = await createServer();
-    await server.ready();
+describe("Server startup", async () => {
+  const app = await createServer();
+  await app.listen({ port: 8080 });
+  it("should start the server successfully", async () => {
+    const request = supertest(app.server);
+    const response = await request.get("/api/users");
+    expect(response.statusCode).toBe(200);
   });
 
-  afterAll(async () => {
-    await server.close();
+  it("should start the server successfully", async () => {
+    const request = supertest(app.server);
+    const response = await request.get(
+      `/api/users/${process.env.USER_ID}/articles/${process.env.TEST_ARTICLE_ID}`
+    );
+    expect(response.statusCode).toBe(200);
   });
 
-  it("will get a 404 for now, will develop further with different routes", async () => {
-    const response = await supertest(server.server).get("/hello");
-    expect(response.statusCode).toBe(404);
+  it("should start the server successfully", async () => {
+    const request = supertest(app.server);
+    const response = await request.get(
+      `/api/users/${process.env.USER_ID}/articles`
+    );
+    expect(response.statusCode).toBe(200);
   });
 });
