90346cf0c3ef896c6f5bb9319463d85b934c2ab7
Joshua Gilley <joshuadgilley@Joshuas-Mac-mini.local>
2025-03-11 14:47:30 -0400

    feat: setting CSP header and anti-clickjacking header

--- server/config/cors.ts
+++ server/config/cors.ts
@@ -9,9 +9,14 @@ const handleCors = (fastify: FastifyInstance<Server>) => {
       "Access-Control-Allow-Headers",
       "Authorization, Origin, X-Requested-With, Content-Type, Accept, X-Slug, X-UID"
     );
+    reply.header("X-Frame-Options", "DENY");
     reply.header(
       "Access-Control-Allow-Methods",
       "OPTIONS, POST, PUT, PATCH, GET, DELETE"
+    );
+    reply.header(
+      "Content-Security-Policy",
+      "default-src 'self'; script-src 'self'; style-src 'self'"
     );
     if (request.method === "OPTIONS") {
       reply.send();
